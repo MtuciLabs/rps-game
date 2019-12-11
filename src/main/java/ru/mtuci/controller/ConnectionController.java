fa86b84670af5e737f037f9e954a082fb6bae4e9
Mansy <shaidullinmansur@gmail.com>
2019-12-11 15:18:30 +0300

    rps

--- src/main/java/ru/mtuci/controller/ConnectionController.java
+++ src/main/java/ru/mtuci/controller/ConnectionController.java
@@ -49,11 +49,10 @@ public class ConnectionController {
   @GetMapping("{gameId}")
   public ResponseEntity<String> connect(@PathVariable("gameId") String gameId) {
     log.info("Connection by gameId={}", gameId);
-    //TODO Что будет если 3ий игрок захочет подключиться?
-    if (gameService.hasGame(gameId)) {
+    //TODO Что будет если 3ий игelрок захочет подключиться?
+    if (gameService.hasGame(gameId) && !gameService.isReadyStartGame(gameId)) {
       return ResponseEntity.ok().build();
     }
-
     return ResponseEntity.notFound().build();
   }
 }
