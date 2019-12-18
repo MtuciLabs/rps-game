e7057d1b147bf7d6fe6f71bf6136761bb861dbba
inesssaketova <inesssaketova@yandex.ru>
2019-12-18 14:11:16 +0300

    TODO выполнены

--- src/main/java/ru/mtuci/websocket/WebSocketUtils.java
+++ src/main/java/ru/mtuci/websocket/WebSocketUtils.java
@@ -11,49 +11,64 @@ import org.springframework.web.socket.WebSocketSession;
  */
 public final class WebSocketUtils {
 
-  public static final Logger log = LoggerFactory.getLogger(WebSocketUtils.class);
+    public static final Logger log = LoggerFactory.getLogger(WebSocketUtils.class);
 
-  public static void sendConnectionMessage(WebSocketSession session, String playerId) {
-    try {
-      String connectionMessage = new JSONObject()
-          .put("id", playerId)
-          .put("type", Type.CONNECTION.toString())
-          .put("connection", true)
-          .toString();
+    public static void sendConnectionMessage(WebSocketSession session, String playerId) {
+        try {
+            String connectionMessage = new JSONObject()
+                    .put("id", playerId)
+                    .put("type", Type.CONNECTION.toString())
+                    .put("connection", true)
+                    .toString();
 
-      if (session.isOpen()) {
-        session.sendMessage(new TextMessage(connectionMessage));
-      }
-    } catch (Exception e) {
-      log.error("Ошибка при отправке сообщения через web-socket.", e);
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(connectionMessage));
+            }
+        } catch (Exception e) {
+            log.error("Ошибка при отправке сообщения через web-socket.", e);
+        }
     }
-  }
 
-  public static void sendResultMessage(WebSocketSession session, String playerId, Result result,
-      PlayerChoice playerChoice) {
-    try {
-      String resultMessage = new JSONObject()
-          .put("id", playerId)
-          .put("type", Type.RESULT.toString())
-          .put("result", result.toString())
-          .put("playerChoice", playerChoice.toString())
-          .toString();
 
-      if (session.isOpen()) {
-        session.sendMessage(new TextMessage(resultMessage));
-      }
-    } catch (Exception e) {
-      log.error("Ошибка при отправке сообщения через web-socket.", e);
+    public static void sendResultMessage(WebSocketSession session, String playerId, Result result,
+                                         PlayerChoice playerChoice) {
+        try {
+            String resultMessage = new JSONObject()
+                    .put("id", playerId)
+                    .put("type", Type.RESULT.toString())
+                    .put("result", result.toString())
+                    .put("playerChoice", playerChoice.toString())
+                    .toString();
+
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(resultMessage));
+            }
+        } catch (Exception e) {
+            log.error("Ошибка при отправке сообщения через web-socket.", e);
+        }
     }
-  }
 
-  public static void sendChatMessage(WebSocketSession session, String textMessage) {
-    try {
-      if (session.isOpen()) {
-        session.sendMessage(new TextMessage(textMessage)); //HtmlUtils.htmlEscape(message)
-      }
-    } catch (Exception e) {
-      log.error("Ошибка при отправке сообщения через web-socket.", e);
+    public static void sendStatusMessage(WebSocketSession session) {
+        try {
+            String statusMessage = new JSONObject()
+                    .put("type", Type.STATUS.toString())
+                    .toString();
+
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(statusMessage));
+            }
+        } catch (Exception e) {
+            log.error("Ошибка при отправке сообщения через web-socket.", e);
+        }
     }
-  }
+
+    public static void sendChatMessage(WebSocketSession session, String textMessage) {
+        try {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(textMessage)); //HtmlUtils.htmlEscape(message)
+            }
+        } catch (Exception e) {
+            log.error("Ошибка при отправке сообщения через web-socket.", e);
+        }
+    }
 }
