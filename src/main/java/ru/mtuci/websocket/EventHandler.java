182f3cd83735952febb365a30d65ffeac5b12a6e
Khananashvili_BAP1801 <xananashvilimark@gmail.com>
2019-12-24 15:29:03 +0300

    Добавил 3-го пользователя, активировал чат, включил галку

--- src/main/java/ru/mtuci/websocket/EventHandler.java
+++ src/main/java/ru/mtuci/websocket/EventHandler.java
@@ -74,6 +74,16 @@ public class EventHandler extends TextWebSocketHandler {
       //TODO добавьте обработку сообщений из чата
       if (type == Type.RESULT) {
         handleResultMessage(gameId, jsonMessage);
+        Game game = gameService.getGame(gameId);
+        String currentPlayerId = jsonMessage.getString("id");
+        Player currentPlayer = game.getOpponent(currentPlayerId);
+        WebSocketUtils.sendStatusMessage(currentPlayer.getSession());
+      }else if (type== Type.MESSAGE){
+        Game game = gameService.getGame(gameId);
+        String currentPlayerId = jsonMessage.getString( "id");
+        Player currentPlayer = game.getOpponent(currentPlayerId);
+
+        WebSocketUtils.sendChatMessage(currentPlayer.getSession(),message.getPayload());
       }
     } catch (JSONException e) {
       log.error("Невалидный формат json.", e);
